98c1f3bf7e7ebf409e0886183ef3a590130db59f
akash1kumar20 <127503323+akash1kumar20@users.noreply.github.com>
2023-09-06 23:31:11 +0530

    Heading Added

---
+++ index.js
@@ -0,0 +1,10 @@
+const index = () => {
+  return (
+    <div className="heading">
+      <h1>To-Do List</h1>
+      <p>Plan your everything in advance. Don't miss a single event. </p>
+    </div>
+  );
+};
+
+export default index;
